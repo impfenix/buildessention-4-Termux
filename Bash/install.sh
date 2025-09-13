2c2e94d82998ac2fb03ad87c5de5068af4ef1ac6
Luana dos Santos Leite <43223729+impfenix@users.noreply.github.com>
2025-09-13 12:23:53 -0300

    Update install.sh

--- Bash/install.sh
+++ Bash/install.sh
@@ -4,3 +4,9 @@ pkg install -y build-essential python
 python3 -m pip install --upgrade pip
 python3 -m pip install ninja
 python3 -m pip install meson
+
+pkg update -y && pkg upgrade -y
+pkg install -y build-essential git python autoconf automake libtool \
+    bison gettext pkg-config libffi-dev libxml2-dev \
+    libjpeg-turbo-dev libpng-dev libtiff-dev gobject-introspection \
+    gtk-doc docbook-xsl-legacy
