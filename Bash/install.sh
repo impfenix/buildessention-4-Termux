b37622036a7311fa0fcf07ff975d1e0d3b45ddbc
Luana dos Santos Leite <43223729+impfenix@users.noreply.github.com>
2025-09-13 00:10:15 -0300

    Update install.sh

--- Bash/install.sh
+++ Bash/install.sh
@@ -1,1 +1,6 @@
-a
+#!/data/data/com.termux/files/usr/bin/bash
+pkg update -y && pkg upgrade -y
+pkg install -y build-essential python
+python3 -m pip install --upgrade pip
+python3 -m pip install ninja
+python3 -m pip install meson
