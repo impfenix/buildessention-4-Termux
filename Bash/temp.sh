b5209c49a95520ae6cefe9cdd8a2508ccbb018af
Luana dos Santos Leite <43223729+impfenix@users.noreply.github.com>
2025-09-13 00:10:39 -0300

    Update temp.sh

--- Bash/temp.sh
+++ Bash/temp.sh
@@ -1,1 +1,11 @@
-a
+#!/data/data/com.termux/files/usr/bin/bash
+set -e
+pkg install -y git gtk3 gettext
+cd ~
+if [ -d "yad" ]; then
+    rm -rf yad
+fi
+git clone https://github.com/v1cont/yad.git
+cd yad
+meson setup build --prefix=$PREFIX
+ninja -C build
